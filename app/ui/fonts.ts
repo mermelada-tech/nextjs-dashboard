79bdc123fc167f5f94e2154e50bf0558fb05cb9b
nadiauj <nadiauj@ext.inditex.com>
2023-11-27 21:35:24 +0100

    chapters 4 & 5

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,6 @@
 import { Inter, Lusitana } from "next/font/google";
 
+
+
 export const inter = Inter({ subsets: ["latin"] });
 export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
